0ba9ae0ecefd7c9024427ff3ca9d97d25803f578
Luis Mario Torres <luisillo77mario@gmail.com>
2023-11-04 16:42:23 -0700

    Creacion de los atributos de la clase muestras

--- CRUD_Rocas/Muestras.cs
+++ CRUD_Rocas/Muestras.cs
@@ -8,5 +8,15 @@ namespace CRUD_Rocas
 {
     internal class Muestras
     {
+        //Atributos
+
+        private int id { get; set; }
+        private string nombre { get; set; }
+        private string tipo { get; set; }
+        private string textura { get; set; }
+        private DateTime fecha { get; set; }
+        //variable para guardar el QAP de la muestra
+        private List<int> QAPF { get; set; }
+        private string caracteristicas { get; set; }
     }
 }
